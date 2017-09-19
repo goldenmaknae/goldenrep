836e4f8828e0df19cb269dd870c0b2c787b467a8
Jungkook <31881675+goldenmaknae@users.noreply.github.com>
2017-09-19 11:57:23 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,8 @@
+##if a == 0:
+##    print('!')
+
+year = int(input())
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print('Високосный')
+else:
+    print('Не високосный')
---
+++ 3.py
@@ -0,0 +1,6 @@
+x = int(input())
+if x % 2 == 0 and x != 0:
+    print('Число чётное')
+else:
+    print('Число нечётное')
+     
