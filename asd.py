beaf1104167eed910a1036ad58a7f88cbf944acd
Jungkook <31881675+goldenmaknae@users.noreply.github.com>
2017-09-26 11:54:08 +0300

    Add files via upload

---
+++ asd.py
@@ -0,0 +1,12 @@
+print('Введите слово: ')
+a = int(input())
+b = int(input())
+s = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+
+l = len(s)
+
+if len(s) == o:
+    while len(s) == 0:
+    print('True')
+
+if 
