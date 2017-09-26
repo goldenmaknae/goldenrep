62c0fbf995bef0102e095c9a17ae2f8411cf5c04
Jungkook <31881675+goldenmaknae@users.noreply.github.com>
2017-09-26 11:31:18 +0300

    Add files via upload

---
+++ dfg.py
@@ -0,0 +1,40 @@
+s = "I love linguistics"
+#print(s[1])
+#i = 2
+#print(s[i])
+
+#print(u)
+
+i = 0
+
+s1 = ""
+
+##if "a" in s:
+##    print(a)
+##elif "o" in s:
+##    print(o)
+
+##for c in s:
+##    if c == "o":
+##        continue
+##    print(c)
+##    i += 1
+##    if c == "i":
+##        break
+##    s1 += c
+##    #print(s)
+##print(s1)
+
+##l = len(s)
+##i = 0
+##
+##while i <= l:
+##    print(s[i])
+##    i += 1
+
+for i,c in enumerate(s):
+    if i == 5:
+        print(c)
+
+for i in range(len(s)):
+    print(s[i])
