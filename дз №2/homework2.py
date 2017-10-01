56b807c4def048ef8076f32f18dd64dd9bdfb1f2
Kookie <31881675+goldenmaknae@users.noreply.github.com>
2017-10-02 00:28:11 +0300

    Add files via upload

---
+++ дз №2/homework2.py
@@ -0,0 +1,10 @@
+print('Введите число:')
+n = int(input())
+
+for n in range(n):
+    print('Введите слово: ')
+    s = input()
+    if s == 'программирование':
+        print('Программа остановлена')
+        break
+print('Конец')
