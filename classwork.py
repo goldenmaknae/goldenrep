6be104bcfbc78c9f13393d86582b61078d4ba33a
Kookie <31881675+goldenmaknae@users.noreply.github.com>
2018-01-11 18:00:46 +0300

    Add files via upload

---
+++ classwork.py
@@ -0,0 +1,18 @@
+text = f.read()
+
+with open("demian.txt", "w", encoding="utf-8"):
+    text = f.read()
+
+    def clean(text):
+        assert type(text) == str
+        string = "',', '.', ':', ';', '!', '?'"
+        for 
+
+    def split_line(text):
+        words = text.split()
+        
+    for word in words:
+        print(word)
+
+    
+        
