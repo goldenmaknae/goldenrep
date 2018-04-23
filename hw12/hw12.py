8ebda8d2ea3b5b1b1d5b9549267eb70d66bb5030
Kookie <31881675+goldenmaknae@users.noreply.github.com>
2018-04-23 04:12:42 +0300

    Add files via upload

---
+++ hw12/hw12.py
@@ -0,0 +1,24 @@
+import os
+
+files = os.listdir()
+folds = ''
+number = 0
+
+for i in range(0, len(files)):
+    path = './' + files[i]
+    valid = False
+    if os.path.isdir(path):
+        for symbol in files[i]:
+            if symbol == ' ':
+                valid = True
+        if valid:
+            number += 1
+            folds += files[i] + ', '
+            
+print('Папок, название которых состоит из более чем одного слова: ', number)
+help = folds.split(',')
+
+del help[-1]
+folds = ', '.join(help)
+
+print('Названия этих папок: ', folds)
