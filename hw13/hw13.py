587356c187d6b4a6ae8418980b831edc1fa27532
Kookie <31881675+goldenmaknae@users.noreply.github.com>
2018-05-21 12:02:36 +0300

    Add files via upload

---
+++ hw13/hw13.py
@@ -0,0 +1,14 @@
+import os
+
+start_path = '.'
+
+for root, dirs, files in os.walk(start_path):
+    print('Где мы находимся: ', root)
+    print('Папки: ', dirs)
+    print('Файлы: ', files)
+
+filenames = {}
+for file in files:
+    filenames[os.path.splitext(file)[0]] = 0
+
+print('Сколько в этих папках встречается разных названий файлов без учёта расширений:', len(filenames))
